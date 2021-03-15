59975fc1a7306bc25bb0522c2b6d7065eecd25ff
Evandro Lippert <evandro.lippert@gmail.com>
2021-03-15 18:18:34 -0300

    Enable Django admin for user profile mode

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
+"""
 
-# Register your models here.
+Para acessar os models criados na página admin do django
+é necessário adicioná-los aqui
+
+"""
+from django.contrib import admin
+from profiles_api import models
+
+admin.site.register(models.UserProfile)
